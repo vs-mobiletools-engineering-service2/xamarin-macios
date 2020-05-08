f165cb41b1ec2849b8a54189a88b8d78cf439e2c
Rolf Bjarne Kvinge <rolf@xamarin.com>
2020-05-08 16:21:01 +0200

    [jenkins] Fix nuget build.

--- jenkins/build-nugets.sh
+++ jenkins/build-nugets.sh
@@ -2,9 +2,6 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-make -C msbuild/dotnet
-make -C msbuild/dotnet pack -j
-
 mkdir -p ../package/
 rm -f ../package/*.nupkg
-cp -c msbuild/dotnet/nupkgs/*.nupkg ../package/
+cp -c dotnet/nupkgs/*.nupkg ../package/
